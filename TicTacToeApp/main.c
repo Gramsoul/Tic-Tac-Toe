91bdc60086cc78ffe6a9715989c8b102f8eb5775
Gram <erebea@live.com.ar>
2025-06-19 19:28:43 -0300

    Se borro rand del main

--- TicTacToeApp/main.c
+++ TicTacToeApp/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "librerias/controApp.h"
 
 
 
 int main()
 {
-    //srand(time(NULL));
     inicializarApp();
     return 0;
 }
